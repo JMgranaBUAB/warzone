3be6b6a4f9b71c63aefa4066dc8295dc5a6b5e07
GranainoBUAB <josemiguel@quesada.eu>
2022-12-04 11:25:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let enviarNivel = document.getElementById("enviarNivel");
+enviarNivel.onclick = newEvent;
+
+async function newEvent() {
+    let nivelRestante = await mostrarNivel();
+    tiempoRestante(nivelRestante);
+}
+
+setInterval(newEvent, 1000);
+
+function mostrarNivel() {
+    let miNivel = document.getElementById("miNivel").value
+    console.log(miNivel)
+
+    let resultado = document.getElementById("resultado")
+    let nivelRestante = 100 - miNivel
+
+    //resultado.innerHTML=`Mi nivel : ${miNivel} <br>`
+    resultado.innerHTML = `Remaining levels :  ${nivelRestante} <br>`
+
+    return (nivelRestante)
+}
+
+function tiempoRestante(nivelRestante) {
+    let ahora = new Date;
+    let limite = new Date("december 1, 2021 6:00:00")/* 08/10/2021 6:00 AM */
+    let diff = limite - ahora
+
+    let resultado = document.getElementById("resultado")
+    let dias = Math.floor((diff / 86400000))
+    let horas = Math.floor((diff - (dias * 86400000)) / 3600000)
+    let minutos = Math.floor((diff - (dias * 86400000) - (horas * 3600000)) / 60000)
+    //resultado.innerHTML+=`Tiempo restante : ${dias}d ${horas}h ${minutos}m <br>`
+
+    let nivelDia = ((nivelRestante / diff) * 86400000).toFixed(5)
+    let nivelHora = ((nivelRestante / diff) * 3600000).toFixed(5)
+    let nivelMinuto = ((nivelRestante / diff) * 60000).toFixed(5)
+
+    resultado.innerHTML += `Levels per day : ${nivelDia} <br>`
+    resultado.innerHTML += `Levels per hour : ${nivelHora} <br>`
+    /* resultado.innerHTML+=`Levels per minute : ${nivelMinuto} <br>` */
+}
+
+
