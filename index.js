eb9ff9d672ba40f2ac7beea059cbdf8b28a01ff9
GranainoBUAB <josemiguel@quesada.eu>
2025-03-04 14:35:51 +0100

    fix: change date end for season 2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function mostrarNivel() {
 
 function tiempoRestante(nivelRestante) {
     let ahora = new Date;
-    let limite = new Date("march 20, 2025 18:00:00")/* 2/1/2023 11:00 AM */
+    let limite = new Date("april 3, 2025 18:00:00")/* 2/1/2023 11:00 AM */
     let diff = limite - ahora
 
     let resultado = document.getElementById("resultado")
